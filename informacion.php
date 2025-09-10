fcb3297aef24fb7ee8766316ad3a94877a650d9f
juanchos04 <juanchos.4928@gmail.com>
2025-09-09 20:39:27 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,11 @@
+<?php
+$identificacion = $_POST['Identificacion'];
+$nomApellidos   = $_POST['NomApellidos'];
+$cargo          = $_POST['cargo'];
+$salario        = $_POST['salario'];
 
+echo "La identificación del empleado es: " . $identificacion . "<br>";
+echo "Los nombres y apellidos del empleado son: " . $nomApellidos . "<br>";
+echo "El cargo del empleado es: " . $cargo . "<br>";
+echo "El salario del empleado es: " . $salario . "<br>";
+?>
